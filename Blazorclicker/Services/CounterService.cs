0eaf1d55e97607e7e29889cad3f34bdeed5fbc7c
Kristjan <kristjan.tepper@gmail.com>
2024-10-15 15:36:48 +0300

    Broken

--- Blazorclicker/Services/CounterService.cs
+++ Blazorclicker/Services/CounterService.cs
@@ -4,27 +4,71 @@ using System;
 
 public class CounterService
 {
-    private int _count;
-    private readonly Timer _timer;
+    private decimal count = 0;
+    private int clickPower = 1;
+    private decimal priceOfClick = 5;
+    private decimal autoClickerPower = 0.1M;
+    private decimal priceOfAutoClicker = 20;
+    private System.Timers.Timer autoClickerTimer;
 
-        public CounterService()
-    {
-        _count = 0;
-        _timer = new Timer(1000); // Timer set for 1 second intervals
-        _timer.Elapsed += OnTimerElapsed;
-        _timer.AutoReset = true; // The timer will continue to run every second
-        _timer.Start(); // Start the timer
-    }
+    public decimal Count => count;
+    public int ClickPower => clickPower;
+    public decimal AutoClickerPower => autoClickerPower;
+    public decimal PriceOfClick => priceOfClick;
+    public decimal PriceOfAutoClicker => priceOfAutoClicker;
 
-    public int Count => _count;
+    public event Action OnChange;
 
+    private void NotifyStateChanged() => OnChange?.Invoke();
     public void IncrementCount()
     {
-        _count++;
+        count = count + (1 * clickPower);
+        NotifyStateChanged(); // Notify UI of state change
+    }
+    public bool BuyClickPower()
+    {
+        if (Count >= priceOfClick)
+        {
+            count -= priceOfClick;
+            clickPower++;
+            priceOfClick = Decimal.Round(priceOfClick * 1.50M, 2);
+            NotifyStateChanged();
+            return true;
+        }
+        return false;
+     }
+
+    public void StartAutoClicker()
+    {
+        autoClickerTimer = new System.Timers.Timer(100);
+        autoClickerTimer.Elapsed += OnAutoClickerEvent;
+        autoClickerTimer.AutoReset = true;
+        autoClickerTimer.Enabled = true;
+
+    }
+    private void OnAutoClickerEvent(object sender, System.Timers.ElapsedEventArgs e)
+    {
+        count += autoClickerPower;
+        NotifyStateChanged();
+    }
+    public bool BuyAutoClickerPower()
+    {
+        if (Count >= priceOfAutoClicker)
+        {
+            count -= priceOfAutoClicker;
+            autoClickerPower+= autoClickerPower+0.1M;
+            priceOfAutoClicker = Decimal.Round(priceOfAutoClicker * 1.50M, 2);
+            if (autoClickerTimer == null)
+            {
+                StartAutoClicker();
+            }
+            NotifyStateChanged();
+            return true;
+        }
+        return false;
+
     }
 
-    private void OnTimerElapsed(object sender, ElapsedEventArgs e)
-    {
-        IncrementCount();
-    }
+
+
 }
